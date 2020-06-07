409d4f3a6b5fd5eb44768910f2058d11190b6252
wzb <wzb3422@gmail.com>
2020-06-07 17:28:49 +0800

    :sparkle: feat: readdir recursively

--- src/upload.js
+++ src/upload.js
@@ -1,10 +1,12 @@
 const OSS = require('ali-oss');
 const chalk = require('chalk');
+const ignore = require('ignore');
 const fs = require('fs');
 
 const log = console.log;
+const cwd = process.cwd();
 
-const validateOpts = (opts) => {
+const validateOpts = (opts, ig) => {
   // default values
   let isForce = false;
 
@@ -21,18 +23,49 @@ const validateOpts = (opts) => {
     log(chalk.yellow('🚨  You are force uploading.'));
   }
 
+  const ignoreArr = fs.readFileSync(opts.ignore).toString().split('\n');
+  ig.add(ignoreArr);
+
   return {
     isForce,
     ossConfig
   }
 }
 
-const uploadToOSS = (ossConfig, isForce) => {
+const uploadToOSS = (ossConfig, files) => {
   const client = new OSS(ossConfig);
+  client.push()
+}
+
+const readDirRecursively = (dirPath, ig) => {
+  let files = [];
+  // console.log(chalk.yellow(`Reading ${dirPath}`));
+  fs.readdirSync(dirPath).forEach(file => {
+    let fullPath = `${dirPath}/${file}`;
+    let filename = fullPath.split('/').slice(-1)[0];
+    // console.log(`fullpath: ${fullPath}`);
+    if (!ig.ignores(filename)) {
+      // console.log(`filename: ${filename}`);
+      if (fs.lstatSync(fullPath).isDirectory()) {
+        files = files.concat(readDirRecursively(fullPath, ig));
+      } else if (fs.lstatSync(fullPath).isFile()) {
+        files.push(fullPath);
+      }
+    } else {
+      console.log(`Ignores ${filename}`);
+    }
+  });
+
+  return files;
 }
 
 const upload = (opts) => {
-  const { isForce, ossConfig } = validateOpts(opts);
+  const ig = ignore();
+  const { isForce, ossConfig } = validateOpts(opts, ig);
+  const files =  readDirRecursively(cwd, ig);
+
 }
+
+
 
 module.exports = upload;
